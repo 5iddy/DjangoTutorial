65ba8d071b6e01e9f62727059124bb909e4781eb
5iddy <siddhuyeturi@gmail.com>
2021-01-26 16:27:33 +0000

    Another update

--- setup.sh
+++ setup.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
-prepare(){
+#COSTANTS
+PROJECT_DIR=$(pwd)
 
-}
+#Installing dependencies
+[ -x "$(command -v virtualenv )"] || pip install virtualenv && virtualenv venv
+
+
+
+$PROJECT_DIR/venv/bin/pip3 install -r requirements.txt
+
+$PROJECT_DIR/venv/bin/python3 $PROJECT_DIR/web/manage.py makemigrations
+$PROJECT_DIR/venv/bin/python3 $PROJECT_DIR/web/manage.py migrate
+$PROJECT_DIR/venv/bin/python3 $PROJECT_DIR/web/manage.py runserver 9090
+
